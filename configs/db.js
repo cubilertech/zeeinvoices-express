60aa77874853cc4f709195a9024228caa7173cdd
Muhammad Waqas <muhammadwaqas3447@gmail.com>
2024-10-25 10:45:15 +0500

    add syncIndex check in mongoose connection

--- configs/db.js
+++ configs/db.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
-
+const Invoice = require("../models/invoice"); // Import your model
+const Client = require("../models/client"); // Import your model
+const Sender = require("../models/sender"); // Import your model
 mongoose
   .connect(process.env.MONGODB_ATLAS_URI, {})
-  .then(() => {
+  .then(async () => {
     console.log("Database Connected.");
+    // Ensure indexes after connection
+    try {
+      await Invoice.syncIndexes();
+      await Client.syncIndexes();
+      await Sender.syncIndexes();
+      console.log("Indexes synchronized.");
+    } catch (err) {
+      console.error("Error synchronizing indexes:", err);
+    }
   })
   .catch((err) => {
     console.log("Error connecting to Database.", err);
