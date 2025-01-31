bf7c89e34111da47ff19621a0000879a40859a17
IceDice <sherazajaz@tutamail.com>
2025-01-31 19:34:13 +0500

    Fixed issue with nodemailer not sending emails

--- services/nodemailer/index.js
+++ services/nodemailer/index.js
@@ -1,14 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USERNAME,
     pass: process.env.SMTP_APP_PASSWORD,
   },
-  tls: {
-    rejectUnauthorized: false,
-  }
+  requireTLS: true,
 });
 
 class NodemailerService {
